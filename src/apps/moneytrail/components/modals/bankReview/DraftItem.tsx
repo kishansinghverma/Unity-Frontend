dc7c66bd83cb144dc12131959852dbd0a7522261
kishansinghverma <kishansinghverma@gmail.com>
2026-05-08 15:01:38 +0530

    refactor: remove dark mode styles from DraftItem component for consistency

--- src/apps/moneytrail/components/modals/bankReview/DraftItem.tsx
+++ src/apps/moneytrail/components/modals/bankReview/DraftItem.tsx
@@ -12,10 +12,10 @@ type DraftItemProps = {
 };
 
 const styles = {
-    container: 'w-full rounded-lg p-2.5 font-sans transition-all duration-300 ease-in-out cursor-pointer relative overflow-hidden group border border-gray-100 dark:border-gray-700 hover:shadow-md dark:hover:shadow-slate-700/40 hover:scale-[1.015] hover:border-slate-200 dark:hover:border-slate-700 [&:not(:first-child)]:mt-3 [&:not(:last-child)]:mb-3',
-    checkCircle: 'w-4 h-4 text-white absolute top-2 right-2 bg-indigo-500 dark:bg-indigo-400 rounded-full p-0.5 shadow',
+    container: 'w-full rounded-lg p-2.5 font-sans transition-all duration-300 ease-in-out cursor-pointer relative overflow-hidden group border border-gray-100 hover:shadow-md hover:scale-[1.015] hover:border-slate-200 [&:not(:first-child)]:mt-3 [&:not(:last-child)]:mb-3',
+    checkCircle: 'w-4 h-4 text-white absolute top-2 right-2 bg-indigo-500 rounded-full p-0.5 shadow',
     primaryText: 'text-sm font-medium line-clamp-2 flex capitalize',
-    datetime: 'flex items-center bg-gray-50 dark:bg-slate-800/60 text-xs font-semibold absolute bottom-2 right-2 px-1.5 py-1',
+    datetime: 'flex items-center bg-gray-50 text-xs font-semibold absolute bottom-2 right-2 px-1.5 py-1',
     clockIcon: 'w-3.5 h-3.5 mr-0.5 opacity-85',
     iconWrapper: 'flex items-start space-x-3'
 };
@@ -23,15 +23,15 @@ const styles = {
 const styleMap = {
     base: {
         textIcon: null,
-        datetime: `text-slate-800 dark:text-slate-100 ${styles.datetime}`,
-        primaryText: `text-slate-500 dark:text-slate-400 ${styles.primaryText}`,
-        container: `bg-gray-50 dark:bg-slate-800/60 shadow-sm dark:shadow-slate-900/30 ${styles.container}`
+        datetime: `text-slate-800 ${styles.datetime}`,
+        primaryText: `text-slate-500 ${styles.primaryText}`,
+        container: `bg-gray-50 shadow-sm ${styles.container}`
     },
     onSelect: {
-        textIcon: 'bg-indigo-100 dark:bg-indigo-700/50 text-indigo-600 dark:text-indigo-300',
-        datetime: `text-indigo-700 dark:text-indigo-300 ${styles.datetime}`,
-        primaryText: `text-indigo-600 dark:text-indigo-400 ${styles.primaryText}`,
-        container: `ring-1 ring-indigo-400 dark:ring-indigo-500 shadow-lg dark:shadow-indigo-900/50 bg-indigo-50/70 dark:bg-slate-800 ${styles.container}`
+        textIcon: 'bg-indigo-100 text-indigo-600',
+        datetime: `text-indigo-700 ${styles.datetime}`,
+        primaryText: `text-indigo-600 ${styles.primaryText}`,
+        container: `ring-1 ring-indigo-400 shadow-lg bg-indigo-50/70 ${styles.container}`
     }
 };
 
